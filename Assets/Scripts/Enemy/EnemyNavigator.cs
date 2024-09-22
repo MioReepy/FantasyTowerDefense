cf785f85ba8233fedcd894a8ac1fa069a53848be
MIO\MioRe_dlqbj0o <mio.reepy@gmail.com>
2024-09-23 00:06:17 +0300

    The "EnemyNavigator" script has been added

---
+++ Assets/Scripts/Enemy/EnemyNavigator.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace EnemySpace
+{
+    public class EnemyNavigator : MonoBehaviour
+    {
+        [SerializeField] private float _movementSpeed = 10f;
+        [SerializeField] private float _rotationSpeed = 100f;
+        [SerializeField] private float _stopDistance = 10f;
+        
+        private Vector3 _destination;
+        private bool _isReachedDestination;
+
+        private void Update()
+        {
+            if (transform.position != _destination)
+            {
+                Vector3 destinationDirection = _destination - transform.position;
+                destinationDirection.y = 0f;
+                
+                float destinationDistance = destinationDirection.magnitude;
+
+                if (destinationDistance >= _stopDistance)
+                {
+                    _isReachedDestination = true;
+                    Quaternion targetRotation = Quaternion.LookRotation(destinationDirection);
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation,
+                        _rotationSpeed * Time.deltaTime);
+                    transform.Translate(Vector3.forward * _movementSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    _isReachedDestination = false;
+                }
+            }
+        }
+
+        internal void SetDestination(Vector3 pointPosition)
+        {
+            _destination = pointPosition;
+            _isReachedDestination = false;
+        }
+    }
+}
