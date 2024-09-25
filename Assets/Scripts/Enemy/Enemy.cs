93c081f995538bacb2d64c339b2d01651ec06847
MIO\MioRe_dlqbj0o <mio.reepy@gmail.com>
2024-09-25 17:22:33 +0300

    Code refactoring

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,16 +2,6 @@ using UnityEngine;
 
 namespace EnemySpace
 {
-    public class Enemy
-    {
-        public int ID;
-
-        public Enemy(EnemyObject enemy)
-        {
-            this.ID = enemy.ID;
-        }
-    }
-
     public class EnemyObject : ScriptableObject
     {
         public int ID;
@@ -19,12 +9,6 @@ namespace EnemySpace
         public EnemyType enemyType;
         [Range(10, 100)] public int enemyHP;
         [Range(0.5f, 10f)] public float enemySpeed;
-
-        public Enemy CreateEnemy()
-        {
-            Enemy newEnemy = new Enemy(this);
-            return newEnemy;
-        }
     }
 }
 
