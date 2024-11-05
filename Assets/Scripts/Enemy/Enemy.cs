0f5e1348963a48960522de3adcf95e58b6dab1dd
MIO\MioRe_dlqbj0o <mio.reepy@gmail.com>
2024-11-06 01:31:02 +0300

    Added animation of enemies

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using WaveSpawnerSpace;
+using WaypointSpase;
 
 namespace EnemySpace
 {
@@ -8,14 +9,37 @@ namespace EnemySpace
         [SerializeField] internal EnemyObject enemy;
         [SerializeField] private GameObject _impactEffect;
         
+        private Animator _animator;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.CompareTag("Bullet"))
             {
                 other.gameObject.SetActive(false);
                 Instantiate(_impactEffect, transform.position, Quaternion.identity);
-                EnemyPool.ReturnToPool(gameObject);
+                _animator.SetBool("IsDied", true);
+                gameObject.GetComponent<Collider>().enabled = false;
+                gameObject.GetComponent<Rigidbody>().useGravity = false;
+                gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                gameObject.GetComponent<WaypointNavigator>().enabled = false;
+                gameObject.GetComponent<EnemyNavigator>().enabled = false;
             }
+        }
+
+        public void EnemyDied()
+        {
+            _animator.SetBool("IsDied", false);
+            gameObject.GetComponent<Collider>().enabled = true;
+            gameObject.GetComponent<Rigidbody>().useGravity = true;
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            gameObject.GetComponent<WaypointNavigator>().enabled = true;
+            gameObject.GetComponent<EnemyNavigator>().enabled = true;
+            EnemyPool.ReturnToPool(gameObject);
         }
     }
 }
