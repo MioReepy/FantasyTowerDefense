2aff79226b2890f7df03256064e474366ce1f0d5
MIO\MioRe_dlqbj0o <mio.reepy@gmail.com>
2024-11-06 16:43:39 +0300

    Added TowerUI display conditions

--- Assets/Scripts/UI/UpgradeTowerUI.cs
+++ Assets/Scripts/UI/UpgradeTowerUI.cs
@@ -34,16 +34,54 @@ namespace UISpace
 
         internal void ShowAvailableBuild()
         {
+            Transform tower = availableBuild.gameObject.transform.GetChild(0).transform;
+
+            for (int i = 0; i < tower.childCount; i++)
+            {
+                SwitchButtons buttons = tower.GetChild(i).transform.GetComponent<SwitchButtons>();
+                int towerCost = tower.GetChild(i).transform.GetComponent<Tower>()
+                    ._towerCost[tower.GetChild(i).transform.GetComponent<Tower>().currentTowerLevel];
+                
+                if (towerCost > PlayerStats.Instance.money)
+                {
+                    buttons.onButton.SetActive(false);
+                    buttons.offButton.SetActive(true);
+                }
+                else
+                {
+                    buttons.onButton.SetActive(true);
+                    buttons.offButton.SetActive(false);
+                }
+            }
+
             availableBuild.gameObject.SetActive(true);
         }
 
         internal void ShowAvailableUpgrade()
         {
+            Transform tower = availableUpgrade.gameObject.transform.GetChild(0).transform;
+
             for (int i = 0; i < _currentTowerButton.childCount; i++)
             {
                 if (_currentTowerButton.GetChild(i).GetComponent<Tower>()._towerType ==
                     gameObject.GetComponent<Builder>().tower._towerType)
                 {
+                    SwitchButtons buttons = tower.GetChild(i).transform.GetComponent<SwitchButtons>();
+                    
+                    int towerCost = tower.GetChild(i).transform.GetComponent<Tower>()
+                        ._towerCost[tower.GetChild(i).transform.GetComponent<Tower>().currentTowerLevel];
+
+                    if (towerCost > PlayerStats.Instance.money)
+                    {
+                        buttons.onButton.SetActive(false);
+                        buttons.offButton.SetActive(true);
+                    }
+                    else
+                    {
+                        buttons.onButton.SetActive(true);
+                        buttons.offButton.SetActive(false);
+                    }
+
                     _currentTowerButton.GetChild(i).gameObject.SetActive(true);
                 }
             }
