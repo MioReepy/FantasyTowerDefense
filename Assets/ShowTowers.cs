2b87d8398e14c6d9296afc5b29d94fc0b5d500f0
MIO\MioRe_dlqbj0o <mio.reepy@gmail.com>
2024-11-08 00:25:49 +0300

    Fixed crash on loading MainMenu from GameOver window on GameScene

--- Assets/ShowTowers.cs
+++ Assets/ShowTowers.cs
@@ -14,9 +14,13 @@ namespace TowerSpace
         {
             for (int i = 0; i < gameObject.transform.childCount; i++)
             {
-                Debug.Log(gameObject.transform.GetChild(i).name);
                 gameObject.transform.GetChild(i).gameObject.SetActive(true);
             }
+        }
+        
+        private void OnDisable()
+        {
+            StartGame.OnStartGame -= StartGame_OnStartGame;
         }
     }
 }
