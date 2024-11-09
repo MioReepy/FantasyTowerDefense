8408b037c179c721d68ea4adccb98ccc10f9705f
MIO\MioRe_dlqbj0o <mio.reepy@gmail.com>
2024-11-09 05:45:53 +0300

    A price bar has been created for the tower purchase window

--- Assets/PrisePlate.cs
+++ Assets/PrisePlate.cs
@@ -1,26 +1,30 @@
 using TMPro;
 using TowerSpace;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PrisePlate : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _priseText;
+    [SerializeField] private TextMeshProUGUI _maxText;
+    [SerializeField] private Image _icon;
     [SerializeField] private GameObject _tower;
 
-    // private void Awake()
-    // {
-    //     if (_tower.GetComponent<TowerInformation>().currentTowerLevel == 0)
-    //     {
-    //         _priseText.text = gameObject.GetComponent<Tower>()._towerObjects[0].towerCost.ToString();
-    //     }
-    //     else if (_tower.GetComponent<TowerInformation>().currentTowerLevel >= _tower.GetComponent<TowerInformation>().towerObjects.Length - 2)
-    //     {
-    //         _priseText.text = "Max";
-    //     }
-    //     else
-    //     {
-    //         _priseText.text = _tower.GetComponent<TowerInformation>().towerObjects[_tower.GetComponent<TowerInformation>().currentTowerLevel].towerCost.ToString();
-    //     }
-    // towerObjects = new TowerObject[_currentTower._towerObjects.Length];
-    // }
+    private void OnEnable()
+    {
+        if (_tower.GetComponent<TowerInformation>().currentTowerLevel == 0)
+        {
+            _priseText.text = gameObject.GetComponent<Tower>()._towerObjects[0].towerCost.ToString();
+        }
+        else if (_tower.GetComponent<TowerInformation>().currentTowerLevel >= _tower.GetComponent<TowerInformation>().towerObjects.Length)
+        {
+            _priseText.gameObject.SetActive(false);
+            _icon.gameObject.SetActive(false);
+            _maxText.gameObject.SetActive(true);
+        }
+        else
+        {
+            _priseText.text = _tower.GetComponent<TowerInformation>().towerObjects[_tower.GetComponent<TowerInformation>().currentTowerLevel].towerCost.ToString();
+        }
+    }
 }
